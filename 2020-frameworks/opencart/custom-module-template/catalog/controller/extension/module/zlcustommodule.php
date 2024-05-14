0bf15e7a6178ee3d2cf1d782c8cdbb5772108e83
Jeff Biaxol <jeff@Jeff.local>
2024-05-14 15:09:45 +0300

    put by curl

--- 2020-frameworks/opencart/custom-module-template/catalog/controller/extension/module/zlcustommodule.php
+++ 2020-frameworks/opencart/custom-module-template/catalog/controller/extension/module/zlcustommodule.php
@@ -5,6 +5,8 @@
 
 class ControllerExtensionModuleZlcustommodule extends Controller {
     private $devmode = true;
+    private $apiDemoUrl = 'https://api.omnisend.com/v3';
+    private $apiDemoKey = '64ae84b8091c7843509cfec3-zVl2WLQUqlyNRgq7fb5sj3xD1zs4aweXF53KB5LG0AZjIPrTsg';
 
     // ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~>> actions
 
@@ -15,41 +17,41 @@ class ControllerExtensionModuleZlcustommodule extends Controller {
     // ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~>> logic
 
     public function operatorSendMessage() {
-
+        $response = $this->requestReplaceCart(
+            '/carts/PostmanTestCheckout1002',
+            [
+                'currency' => 'EUR',
+                'intTotal' =>  100
+            ]
+        );
     }
 
     // ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~>> requests
 
-    public function requestReplace(
-        $path = '/',
-        $body = [],
+    public function requestReplaceCart(
+        $endpoint = '/',
+        $body = []
     ) {
-        $endpointApiUrlCheckout = 'https://api.omnisend.com/v3/carts/';
-        $endpointApiKey = '64ae84b8091c7843509cfec3-zVl2WLQUqlyNRgq7fb5sj3xD1zs4aweXF53KB5LG0AZjIPrTsg';
-        $endpointTargetID = 'PostmanTestCheckout1002';
-        $body = [
-            'currency' => $data['currency'],
-            'cartSum'  => $data['cartSum'],
-            'products' => $data['products']
-        ];
+        // PUT BY CURL
         $curl = curl_init();
         curl_setopt_array($curl, [
-            CURLOPT_URL => $endpointApiUrlCheckout . $endpointTargetID,
+            CURLOPT_URL => "" . $this->apiDemoUrl . $endpoint,
             CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_ENCODING => "",
-            CURLOPT_MAXREDIRS => 10,
-            CURLOPT_TIMEOUT => 30,
-            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-            CURLOPT_CUSTOMREQUEST => "PUT",
-            CURLOPT_POSTFIELDS => json_encode($body),
-            CURLOPT_HTTPHEADER => [
-                "X-API-KEY: " . $endpointApiKey,
-                "accept: application/json",
-                "content-type: application/json"
+            CURLOPT_ENCODING       => "",
+            CURLOPT_MAXREDIRS      => 10,
+            CURLOPT_TIMEOUT        => 30,
+            CURLOPT_HTTP_VERSION   => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST  => "PUT",
+            CURLOPT_POSTFIELDS     => json_encode($body),
+            CURLOPT_HTTPHEADER     => [
+                "X-API-KEY: " . $this->apiDemoKey,
+                "accept:        application/json",
+                "content-type:  application/json"
             ],
         ]);
         $response = curl_exec($curl);
         curl_close($curl);
+        return $response;
     }
 
 
