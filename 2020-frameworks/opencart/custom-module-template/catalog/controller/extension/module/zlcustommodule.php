1cc6bb9d0e880ec8e88d8a261071aa1cbff336f6
Jeff Biaxol <jeff@Jeff.local>
2024-05-14 16:03:48 +0300

    requestRun

--- 2020-frameworks/opencart/custom-module-template/catalog/controller/extension/module/zlcustommodule.php
+++ 2020-frameworks/opencart/custom-module-template/catalog/controller/extension/module/zlcustommodule.php
@@ -17,42 +17,99 @@ class ControllerExtensionModuleZlcustommodule extends Controller {
     // ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~>> logic
 
     public function operatorSendMessage() {
-        $response = $this->requestReplaceCart(
-            '/carts/PostmanTestCheckout1002',
+        $checkoutID    = $this->getCheckoutID();
+        $checkoutInfo  = $this->getCheckoutInfo($checkoutID);
+        $checkoutItems = $this->getCheckoutItems($checkoutID);
+
+        $response = $this->requestUpdateCart(
+            $checkoutID,
             [
-                'currency' => 'EUR',
-                'intTotal' =>  100
+                'currency' => $checkoutInfo['currency'],
+                'cartSum'  => $checkoutInfo['cartSum'],
+                'products' => $checkoutItems
             ]
         );
     }
 
-    // ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~>> requests
-
-    public function requestReplaceCart(
-        $endpoint = '/',
+    public function requestUpdateCart(
+        $cart = '',
         $body = []
     ) {
-        // PUT BY CURL
+        $path = $this->apiDemoUrl.'/carts/'.$cart;
+        $xkey = $this->apiDemoKey;
+        $response = $this->requestRun('PUT', $path, $body, $xkey);
+        return $response;
+    }
+
+    // ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~>> data
+
+    private function getCheckoutID() {
+        $checkoutID = $this->session->data['order_id'];
+        return $checkoutID;
+    }
+
+    private function getCheckoutInfo() {
+        $this->load->model('checkout/order');
+        $checkoutID = $this->getCheckoutID();
+        $checkoutOrderBase = $this->model_checkout_order->getOrder($checkoutID);
+        $checkoutOrderData = $this->request->post['order_data'];
+        return [
+            'email'    => $checkoutOrderData['email'],
+            'cartSum'  => round($checkoutOrderBase['total'] * 100),
+          //'cartSum'  => round($checkoutOrderData['total'] * 100),
+            'currency' => 'EUR'
+        ];
+    }
+
+    public function getCheckoutItems($checkoutID) {
+        $this->load->model('checkout/order');
+        $checkoutShopItems = $this->model_checkout_order->getOrderProducts($checkoutID);
+        $checkoutItems = array_map(
+            function ($index, $product) {
+                return [
+                    'cartProductID' => $product['order_product_id'],
+                    'productID'     => $product['product_id'],
+                    'variantID'     => $product['product_id'],
+                    'title'         => $product['name'],
+                    'description'   => $product['model'],
+                    'price'         => round($product['price'] * 100),
+                    'quantity'      => round($product['quantity'])
+                ];
+            },
+            array_keys($checkoutShopItems),
+            array_values($checkoutShopItems)
+        );
+        return $checkoutItems;
+    }
+
+    // ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~>> workers
+
+    public function requestRun(
+        $type = 'POST',
+        $path = '',
+        $body = [],
+        $xkey = ''
+    ) {
         $curl = curl_init();
         curl_setopt_array($curl, [
-            CURLOPT_URL => "" . $this->apiDemoUrl . $endpoint,
+            CURLOPT_URL            => $path,
+            CURLOPT_CUSTOMREQUEST  => $type,
+            CURLOPT_POSTFIELDS     => json_encode($body),
+            CURLOPT_HTTPHEADER     => [
+                "X-API-KEY: " . $xkey,
+                "accept:        application/json",
+                "content-type:  application/json"
+            ],
             CURLOPT_RETURNTRANSFER => true,
             CURLOPT_ENCODING       => "",
             CURLOPT_MAXREDIRS      => 10,
             CURLOPT_TIMEOUT        => 30,
             CURLOPT_HTTP_VERSION   => CURL_HTTP_VERSION_1_1,
-            CURLOPT_CUSTOMREQUEST  => "PUT",
-            CURLOPT_POSTFIELDS     => json_encode($body),
-            CURLOPT_HTTPHEADER     => [
-                "X-API-KEY: " . $this->apiDemoKey,
-                "accept:        application/json",
-                "content-type:  application/json"
-            ],
         ]);
         $response = curl_exec($curl);
+        $responseErorr = curl_error($curl);
         curl_close($curl);
-        return $response;
+        return $response ? $response : $responseErorr;
     }
-
 
 }
